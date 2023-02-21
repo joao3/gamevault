1b58fcdc59ab9d88db208da5b7754daecca2e967
joao0otavio3@gmail.com <joao0otavio3@gmail.com>
2023-02-21 20:16:08 -0300

    adds igdb request function

---
+++ server/src/services/igdb.ts
@@ -0,0 +1,21 @@
+import fetch, { Response } from 'node-fetch';
+
+require('dotenv').config();
+
+const igdbRequest = async (query: string): Promise<Response> => {
+  const requestConfig = {
+    method: 'post',
+    body: `${query}`,
+    headers: {
+      'Content-Type': 'text/plain',
+      'Client-ID': `${process.env.IGDB_CLIENT_ID}`,
+      'Authorization': `${process.env.IGDB_AUTHORIZATION}`
+    }
+  };
+
+  const response = await fetch('https://api.igdb.com/v4/games/', requestConfig);
+
+  return response;
+}
+
+export { igdbRequest };
