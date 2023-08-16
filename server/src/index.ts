c1e5e8709e0588274a816d948d979b5e8327285c
Joao Otavio <joao0otavio3@gmail.com>
2023-08-16 17:00:46 -0300

    remove type error

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes/routes';
 
-const a: string = 1;
-
-// eslint-disable-next-line no-console
-console.log(a);
-
 const port = 8000;
 
 const app = express();
